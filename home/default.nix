331975018d3ae4f4e2aacf0ddd5031f1de9650cb
tb <tb@cirqle.no>
2026-03-16 01:42:32 +0100

    From brew to Nix busy work

--- home/default.nix
+++ home/default.nix
@@ -1,20 +1,21 @@
 { pkgs, ... }:
-{
-  imports = [
-    ./git.nix
-    ./zsh.nix
-  ];
-
-  home.username = "tb";
-  home.homeDirectory = "/Users/tb";
-
-  # Never change this after first activation
-  home.stateVersion = "24.11";
-
-  # Let Home Manager manage itself
-  programs.home-manager.enable = true;
-
-  home.packages = [
-  pkgs.qbittorrent
-];
-}
+  {
+    imports = [
+      ./git.nix
+      ./zsh.nix
+    ];
+    
+    home.username = "tb";
+    home.homeDirectory = "/Users/tb";
+    
+    # Never change this after first activation
+    home.stateVersion = "24.11";
+    
+    # Let Home Manager manage itself
+    programs.home-manager.enable = true;
+    
+    home.packages = [
+    pkgs.qbittorrent
+    pkgs.tmuxai
+    ];
+  }
